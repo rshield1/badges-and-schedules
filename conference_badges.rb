93145437375dd43efb347de91c78a75f3dcebe63
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-11 01:47:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,8 @@ end
 
 def assign_rooms(names)
   
-  name.each_with_index.map |name, index|
+  name.each_with_index.map |name|
+  
     puts "Hello, #{name}! You'll be assigned to room #{num}!"
   end 
 end
